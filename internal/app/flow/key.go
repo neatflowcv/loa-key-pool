86a835b84c8bcca3ccd424094bddab000340d767
biosvos <biosvos@gmail.com>
2025-10-26 21:01:10 +0900

    flow skeleton

---
+++ internal/app/flow/key.go
@@ -0,0 +1,7 @@
+package flow
+
+type Key struct {
+	ID          string
+	Key         string
+	Description string
+}
--- internal/app/flow/service.go
+++ internal/app/flow/service.go
@@ -1,7 +1,29 @@
 package flow
 
+import (
+	"context"
+	"errors"
+)
+
 type Service struct{}
 
 func NewService() *Service {
 	return &Service{}
+}
+
+// CreateKey returns key id
+func (s *Service) CreateKey(ctx context.Context, key string, description string) (*Key, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *Service) ListKeys(ctx context.Context) ([]*Key, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (s *Service) DeleteKey(ctx context.Context, id string) error {
+	return errors.New("not implemented")
+}
+
+func (s *Service) PickKey(ctx context.Context) (*Key, error) {
+	return nil, errors.New("not implemented")
 }
